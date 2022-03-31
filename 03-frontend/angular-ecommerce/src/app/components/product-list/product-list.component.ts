a3daf42e8db0e67f97c0f4288f21b65149deecc7
Israel Mesa <mesa.israel50@gmail.com>
2022-03-30 22:24:27 -0400

    Display Category name in Produc List challenge

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number | undefined;
+  currentCategoryName: String | undefined;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -27,13 +28,18 @@ export class ProductListComponent implements OnInit {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
-      // get the "id" param string. convert string to a number
+      // get the "id" param string. convert string to a number using the "+" symbol
       // @ts-ignore
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+
+      // get the "name" param string
+      // @ts-ignore
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     }
     else {
       // category id not available...default to category id 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books'
     }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(
