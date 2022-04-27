a051841516b75f4d898cb47a900d073318528ba5
Israel Mesa <mesa.israel50@gmail.com>
2022-04-26 23:29:11 -0400

    refactor to find method

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -24,12 +24,8 @@ export class CartService {
     if (this.cartItems.length > 0) {
       // find the item in the cart based on item id
 
-      for (let tempCartItem of this.cartItems) {
-        if (tempCartItem.id === theCartItem.id) {
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
+      // @ts-ignore
+      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id);
 
       // check if we found it
       alreadyExistsInCart = (existingCartItem != undefined);
